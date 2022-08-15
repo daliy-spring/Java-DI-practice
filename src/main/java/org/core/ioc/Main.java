75043ade89c20f6470dbe4b722d3181050c0a97d
Haneul Lee <hee1149kr@naver.com>
2022-08-15 11:51:34 +0900

    feat: spring di concept(#1)

--- src/main/java/org/core/ioc/Main.java
+++ src/main/java/org/core/ioc/Main.java
@@ -2,6 +2,12 @@ package org.core.ioc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "www.naver.com/book/it?page=10&size=20&name=spring-boot";
+
+        // 외부에서 사용하는 객체를 주입받는 형태를 DI라고 한다. (의존성 주입)
+        Encoder encoder = new Encoder(new UrlEncoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
+
     }
 }
